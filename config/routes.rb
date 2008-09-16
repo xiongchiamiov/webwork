506b31e81294ce38dd8e956afb05d5e0db3e5c22
James Pearson <james@xyztextbooks.com>
2008-09-16 15:47:55 -0700

    a few adjustments to get working on production server

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id', :controller => 'wml'
   map.connect ':controller/:action/:id.:format'
 end
